e4d98bb31569a8a239618c7d5f356d6389653080
Nikola Argirov <niki0598@yahoo.com>
2019-07-05 19:15:02 +0300

    Test 3

--- Test1/Form1.Designer.vb
+++ Test1/Form1.Designer.vb
@@ -28,6 +28,9 @@ Partial Class Form1
         Me.CheckBox2 = New System.Windows.Forms.CheckBox()
         Me.CheckBox3 = New System.Windows.Forms.CheckBox()
         Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label2 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'TextBox1
@@ -85,11 +88,41 @@ Partial Class Form1
         Me.ComboBox1.Size = New System.Drawing.Size(235, 21)
         Me.ComboBox1.TabIndex = 5
         '
+        'ListView1
+        '
+        Me.ListView1.Location = New System.Drawing.Point(796, 388)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(121, 97)
+        Me.ListView1.TabIndex = 6
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Location = New System.Drawing.Point(781, 280)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(149, 102)
+        Me.GroupBox1.TabIndex = 7
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "GroupBox1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 40.25!)
+        Me.Label2.Location = New System.Drawing.Point(770, 192)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(177, 63)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Test2!"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1151, 670)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.ListView1)
         Me.Controls.Add(Me.ComboBox1)
         Me.Controls.Add(Me.CheckBox3)
         Me.Controls.Add(Me.CheckBox2)
@@ -109,4 +142,7 @@ Partial Class Form1
     Friend WithEvents CheckBox2 As CheckBox
     Friend WithEvents CheckBox3 As CheckBox
     Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents ListView1 As ListView
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label2 As Label
 End Class
